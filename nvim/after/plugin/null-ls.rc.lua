b809f624c5f970c0f21142b67b3e415e543b9704
Jingnan si <jingnan.si@gmail.com>
2022-08-28 11:17:56 -0700

    change clang-format to uncrustify, add emacs-conifg for emacs init

--- nvim/after/plugin/null-ls.rc.lua
+++ nvim/after/plugin/null-ls.rc.lua
@@ -3,14 +3,14 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.yapf,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.uncrustify,
   },
-  
+
   on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
 
