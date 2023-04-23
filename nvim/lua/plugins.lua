404648b2bdb86fcc2788cedf59086d117891e201
Jingnan si <jingnan.si@gmail.com>
2023-04-23 09:17:04 -0700

    migrate from packer to lazy

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,34 +1,36 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+return require('lazy').setup({
+  'nvim-lua/plenary.nvim',
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
-
-  use {
+  {
     'kyazdani42/nvim-web-devicons',
     config = function()
       require('nvim-web-devicons').setup({
         default = true
       })
     end
-  }
+  },
 
-  use 'equalsraf/neovim-gui-shim'
+  'equalsraf/neovim-gui-shim',
 
-  use 'nvim-treesitter/nvim-treesitter'
+  'nvim-treesitter/nvim-treesitter',
 
-  use 'bronson/vim-crosshairs'
-  use 'editorconfig/editorconfig-vim'
+  'bronson/vim-crosshairs',
+  'editorconfig/editorconfig-vim',
 
-  use {
+  {
     'nvim-lualine/lualine.nvim',
     config = function()
       require('lualine').setup {
@@ -37,77 +39,78 @@ return require('packer').startup(function(use)
         }
       }
     end
-  }
+  },
 
-  use {
+  {
     'karb94/neoscroll.nvim',
     config = function()
       require('neoscroll').setup()
     end
-  }
+  },
 
-  use {
+  {
     'ahmedkhalf/project.nvim',
     config = function()
       require('project_nvim').setup({
 
       })
     end
-  }
+  },
 
-  use {
+  {
     'phaazon/hop.nvim',
     config = function()
       require('hop').setup()
     end
-  }
+  },
 
-  use 'akinsho/nvim-bufferline.lua'
+  'akinsho/nvim-bufferline.lua',
 
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use {
+  'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  {
     'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'make'
-  }
+    build = 'make'
+  },
 
-  use {
+  {
     'windwp/nvim-autopairs',
     config = function()
       require('nvim-autopairs').setup({
         disable_filetype = { "TelescopePrompt" , "vim" },
       })
     end
-  }
-  use {
+  },
+
+  {
     'windwp/nvim-ts-autotag',
     config = function()
       require('nvim-ts-autotag').setup()
     end
-  }
+  },
 
   -- themes
-  use {
+  {
     'norcalli/nvim-colorizer.lua',
     config = function()
       require('colorizer').setup({
         '*';
       })
     end
-  }
+  },
 
-  use {
+  {
     'Mofiqul/dracula.nvim',
     config = function()
       require('dracula').setup()
     end
-  }
+  },
 
   -- LSP
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  'neovim/nvim-lspconfig',
+  'jose-elias-alvarez/null-ls.nvim',
 
-  use {
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     requires = {
@@ -128,10 +131,10 @@ return require('packer').startup(function(use)
       {'L3MON4D3/LuaSnip'},             -- Required
       {'rafamadriz/friendly-snippets'}, -- Optional
     }
-  }
+  },
 
   -- Prettier plugin for Neovim's built-in LSP client
-  use {
+  {
     'MunifTanjim/prettier.nvim',
     config = function()
       require('prettier') .setup({
@@ -148,24 +151,24 @@ return require('packer').startup(function(use)
         }
       })
     end
-  }
+  },
 
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  'glepnir/lspsaga.nvim', -- LSP UIs
 
   -- yank ring UI
-  use 'gbprod/yanky.nvim'
+  'gbprod/yanky.nvim',
 
   -- Guess indent
-  use 'tpope/vim-sleuth'
+  'tpope/vim-sleuth',
 
   -- Move the range instead of copy paste
-  use 'matze/vim-move'
+  'matze/vim-move',
 
   -- incremental search enhance
-  use 'haya14busa/is.vim'
+  'haya14busa/is.vim',
 
   -- show current indent
-  use {
+  {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
       require('indent_blankline').setup({
@@ -173,37 +176,30 @@ return require('packer').startup(function(use)
         show_current_context_start = true,
       })
     end
-  }
+  },
 
   -- deal with trail white space
-  use {
+  {
     'ntpeters/vim-better-whitespace',
     config = function()
       vim.g.strip_only_modified_lines = 1
       vim.g.strip_whitespace_on_save = 1
     end
-  }
+  },
 
   -- remove search hl after cusor move or mode change
-  use 'romainl/vim-cool'
+  'romainl/vim-cool',
 
   -- improve startup time
-  use {
+  {
     'lewis6991/impatient.nvim',
-  }
+  },
 
-  use {
+  {
     'folke/which-key.nvim',
     config = function()
       require('which-key').setup()
     end
-  }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-
-end)
+  },
+})
 
