4f9feebb73c95bc7046f72274e579f01061edd43
Jingnan si <jingnan.si@gmail.com>
2023-05-17 14:34:09 -0700

    add wezterm

---
+++ wezterm/launcher.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm'
+local launch_menu = {}
+
+local function init_module()
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  table.insert(launch_menu, {
+    label = 'PowerShell',
+    args = { 'powershell.exe', '-NoLogo' },
+  })
+
+  -- Find installed visual studio version(s) and add their compilation
+  -- environment command prompts to the menu
+  for _, vsvers in
+    ipairs(
+      wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+    )
+  do
+    local year = vsvers:gsub('Microsoft Visual Studio/', '')
+    table.insert(launch_menu, {
+      label = 'x64 Native Tools VS ' .. year,
+      args = {
+        'cmd.exe',
+        '/k',
+        'C:/Program Files (x86)/'
+          .. vsvers
+          .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+      },
+    })
+  end
+end
+end
+
+local module = {}
+
+function module.apply_to_config(config)
+  init_module()
+  config.launch_menu = launch_menu
+end
+
+return module
