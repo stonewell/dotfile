658e2fd78a7376b201ce8e50be50686c28cd35ce
Jingnan si <jingnan.si@gmail.com>
2024-03-21 21:30:26 -0700

    add counsel for app-launcher

--- emacs.d/lisp/as-emacs-packages.el
+++ emacs.d/lisp/as-emacs-packages.el
@@ -208,5 +208,28 @@
     :args '("/dev/stdin"))
   )
 
+(use-package counsel
+  :ensure t
+  :config
+
+  (defun emacs-counsel-launcher ()
+    "Create and select a frame called emacs-counsel-launcher which consists only of a minibuffer and has specific dimensions. Runs counsel-linux-app on that frame, which is an emacs command that prompts you to select an app and open it in a dmenu like behaviour. Delete the frame after that command has exited"
+    (interactive)
+    (with-selected-frame
+      (make-frame '((name . "emacs-run-launcher")
+                     (minibuffer . only)
+                     (fullscreen . 0) ; no fullscreen
+                     (undecorated . t) ; remove title bar
+                     ;;(auto-raise . t) ; focus on this frame
+                     ;;(tool-bar-lines . 0)
+                     ;;(menu-bar-lines . 0)
+                     (internal-border-width . 10)
+                     (width . 80)
+                     (height . 11)))
+      (unwind-protect
+        (counsel-linux-app)
+        (delete-frame))))
+  )
+
 (provide 'as-emacs-packages)
 ;;; as-emacs-packages.el ends here
