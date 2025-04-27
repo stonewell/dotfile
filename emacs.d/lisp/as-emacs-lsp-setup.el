62ecb07baacd64ba2bc76f1ed0e86309c5ab76d0
Jingnan si <jingnan.si@gmail.com>
2025-04-26 18:56:07 -0700

    update emacs config. add mermaid docker file

--- emacs.d/lisp/as-emacs-lsp-setup.el
+++ emacs.d/lisp/as-emacs-lsp-setup.el
@@ -1,10 +1,47 @@
-;; -*- lexical-binding: t; -*-
+;;; package --- as-emacs-lsp-setup setup lsp package -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 (use-package lsp-mode
   :ensure t
   :defer t
+  :custom
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  :init
-  (setq lsp-keymap-prefix "C-c l")
+  (lsp-keymap-prefix "C-c l")
+  (lsp-session-file (expand-file-name ".lsp-session" user-emacs-directory))
+  (lsp-log-io nil)
+  (lsp-keep-workspace-alive nil)
+  (lsp-idle-delay 0.5)
+  ;; core
+  (lsp-enable-xref t)
+  (lsp-eldoc-enable-hover nil)
+  (lsp-enable-dap-auto-configure nil)
+  (lsp-enable-file-watchers nil)
+  (lsp-enable-folding nil)
+  (lsp-enable-imenu nil)
+  ;;(lsp-enable-indentation nil)
+  (lsp-enable-links nil)
+  (lsp-enable-on-type-formatting nil)
+  (lsp-enable-symbol-highlighting nil)
+  (lsp-enable-text-document-color nil)
+  ;; completion
+  (lsp-completion-enable t)
+  (lsp-completion-enable-additional-text-edit nil)
+  (lsp-enable-snippet nil)
+  (lsp-completion-show-kind nil)
+  ;; headerline
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-headerline-breadcrumb-enable-diagnostics nil)
+  (lsp-headerline-breadcrumb-enable-symbol-numbers nil)
+  (lsp-headerline-breadcrumb-icons-enable nil)
+  ;; modeline
+  (lsp-modeline-code-actions-enable nil)
+  (lsp-modeline-diagnostics-enable nil)
+  (lsp-modeline-workspace-status-enable nil)
+  (lsp-signature-doc-lines 1)
+  ;; lens
+  (lsp-lens-enable nil)
+  ;; semantic
+  (lsp-semantic-tokens-enable nil)
   :hook (
           ((c-or-c++-mode c-or-c++-ts-mode rust-ts-mode)  . lsp-deferred)
           ;; if you want which-key integration
@@ -23,10 +60,11 @@
   :ensure t
   :defer t
   :commands helm-lsp-workspace-symbol)
+
 (use-package lsp-treemacs
   :ensure t
   :defer t
   :commands lsp-treemacs-errors-list)
 
 (provide 'as-emacs-lsp-setup)
-;;
+;;; as-emacs-lsp-setup.el ends here
