bd4dab1ab20b44ad3ab298f7690fb103650b09e5
Jingnan si <jingnan.si@gmail.com>
2026-01-17 22:03:17 -0800

    update to latest config

--- textadept/init.lua
+++ textadept/init.lua
@@ -29,10 +29,11 @@ view.margin_width_n[3] = 0
 
 ui.command_entry.caret_period = 0
 ui.command_entry.caret_style = view.CARETSTYLE_BLOCK
-ui.command_entry.caret_line_frame = 1
+ui.command_entry.caret_line_frame = 0
 view.caret_period = 0
 view.caret_style = view.CARETSTYLE_BLOCK
-view.caret_line_frame = 1
+view.caret_line_frame = 0
+
 -- keep caret 8 lines away from top/bottom
 view:set_y_caret_policy(view.CARET_SLOP | view.CARET_STRICT | view.CARET_EVEN, 8)
 
@@ -41,15 +42,17 @@ view.edge_color = 0xcccccc
 
 view.indentation_guides = buffer.IV_LOOKBOTH
 view.whitespace_size = 3
+view.highlight_guide = 1
 
 io.ensure_final_newline = true
 view.end_at_last_line = false
 
 -- textadept.editing.highlight_words = textadept.editing.HIGHLIGHT_SELECTED
 -- center results when searching
-events.connect(events.FIND_RESULT_FOUND, function()
-                                             view:vertical_center_caret()
-                                         end)
+events.connect(events.FIND_RESULT_FOUND,
+               function()
+                 view:vertical_center_caret()
+               end)
 
 -- Reset selection color
 events.connect(events.VIEW_NEW,
@@ -81,27 +84,7 @@ keys['shift+ctrl+\t'] = nil
 
 emacs.keys.enable()
 
-local fn_dispatch = {
-    ['open'] = textredux.fs.open_file,
-    ['switchbuffer'] = textredux.buffer_list.show,
-    ['saveas'] = textredux.fs.save_buffer_as,
-    -- ['recent'] = textredux.core.filteredlist.wrap(io.open_recent_file),
-    -- ['switchbuffer_project'] = textredux.core.filteredlist.wrap(util.show_project_buffers),
-
-    ['open_recent'] = io.open_recent_file,
-}
-
-keys['ctrl+x'] = {
-  ['0'] = function() view:unsplit() end,
-  ['b'] = fn_dispatch['switchbuffer'],
-  ['k'] = fn_dispatch['switchbuffer'],
-  ['ctrl+c'] = quit,
-  ['ctrl+f'] = fn_dispatch['open'],
-  ['ctrl+r'] = fn_dispatch['open_recent'],
-  ['ctrl+s'] = buffer.save,
-  ['ctrl+w'] = fn_dispatch['saveas'],
-}
-
 keys['alt+o'] = function()
     isearch.replace.start_replace()
 end
+
