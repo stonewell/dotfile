ae4f6da2570c5feaf23017a557c9b6f47dd2f98d
Jingnan si <jingnan.si@gmail.com>
2026-05-12 14:57:14 -0700

    update emacs and hyprland

--- emacs.d/lisp/as-emacs-packages.el
+++ emacs.d/lisp/as-emacs-packages.el
@@ -276,5 +276,23 @@
   (setq split-window-preferred-function 'split-window-prefer-horizonally)
   )
 
+(use-package buffer-terminator
+  :ensure t
+  :demand
+  :custom
+  (buffer-terminator-verbose nil)
+
+  ;; Set the inactivity timeout (in seconds) after which buffers are considered
+  ;; inactive (default is 30 minutes):
+  (buffer-terminator-inactivity-timeout (* 30 60)) ; 30 minutes
+
+  ;; Define how frequently the cleanup process should run (default is every 10
+  ;; minutes):
+  (buffer-terminator-interval (* 10 60)) ; 10 minutes
+
+  :config
+  (buffer-terminator-mode 1))
+
+
 (provide 'as-emacs-packages)
 ;;; as-emacs-packages.el ends here
